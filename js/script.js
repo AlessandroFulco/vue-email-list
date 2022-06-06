4bf399662ae41caf91668f8a0dcd4fd4b0e90b87
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-06-06 17:38:50 +0200

    finalizzazione

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ const app = new Vue({
     },
     mounted(){
         for (let i = 0; i < 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(function (result) {
-                const emailAdd = result.data.response;
-                this.emailAdd.push(email);
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((result) => {
+                const myResult = result.data.response;
+                this.email.push(myResult);
             });
+            
         }
     }
 
